c2345c3a01cfd247de7eb274f81c43b7661c904f
root <root@debian>
2020-04-06 23:55:14 -0300

    Nova alteração na aplicação usuario

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['localhost','192.168.100.5','ansecommerce.herokuapp.com']
 
