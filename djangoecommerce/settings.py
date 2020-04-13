24d6a4b22a3bedd21765cf74eb936c52ba1ce5b3
root <root@debian>
2020-04-12 21:09:35 -0300

    Checkout

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['ansecommerce.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','ansecommerce.herokuapp.com']
 
 
 # Application definition
